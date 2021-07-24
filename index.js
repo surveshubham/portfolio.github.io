b88dd3e71b38b91f91e04412db19d369a3dcecc1
surveshubham <87568048+surveshubham@users.noreply.github.com>
2021-07-24 18:28:26 +0530

    uploaded raw files
    raw files are uploaded in this folder

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("hello i am working");
+
+
+function scrollWin1() {
+    window.scrollTo(0,0);
+  }
+
+function scrollWin2() {
+  window.scrollTo(0,710);
+}
+
+function scrollWin3() {
+    window.scrollTo(0,1300);
+  }
+
+
